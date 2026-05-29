09422a93189d45265178c55871dfab88ff42e11a
Khangaikhuu Uvgunkhuu <eegii@gmail.com>
2026-05-29 16:57:17 -0700

    test

--- AnimalPark.java
+++ AnimalPark.java
@@ -184,6 +184,14 @@ public class AnimalPark {
             if (a instanceof Elephant e) e.spray();
             if (a instanceof Penguin p) p.swim();
             if (a instanceof Giraffe g) g.eatLeaves();
+            
+            class Keeper {
+    String name;
+    Animal[] assigned;  // хариуцсан амьтдын жагсаалт
+
+    void feedAssigned(String food) { ... }
+    void checkAllHealth() { ... }
+}
         }
     }
 }
